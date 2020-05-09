f3028b3db3d50fd0330792b60c48e9decab32cde
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-05-09 11:49:08 -0700

    Using MongoDB instead of Massive.js and Heroku PostgreSQL. Message endpoints working

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,7 +1,19 @@
 module.exports = {
     getMessages: async (req, res, next) => {
+        const db = req.app.get('db')
+        db.collection('messages').find().toArray()
+                .then(results => {
+                    res.status(200).send(results)
+                })
+                .catch(error => console.error(error));
+    },
+
+    addMessage: async (req, res) => {
         const db = req.app.get('db');
-        const messages = await db.get_messages;
-        res.status(200).send(messages);
+        db.collection('messages').insertOne(req.body)
+            .then(result => {
+                res.sendStatus(200);
+            })
+            .catch(error => console.error(error));
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
-const massive = require('massive');
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const { getMessages, addMessage } = require('./controllers/messageController');
 
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 const app = express();
 
+app.use(require('body-parser').text());
 app.use(express.json());
 
-massive(CONNECTION_STRING).then(db => {
-    console.log('Connected to database.');
-    app.set('db', db);
-}).catch(err => console.log(err));
+MongoClient.connect(CONNECTION_STRING, {useUnifiedTopology: true})
+    .then(client => {
+        console.log('Connected to databse.');
+        app.set('db', client.db('wedding'));
 
-app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
+        app.get('/api/messages', getMessages);
+        app.post('/api/message', addMessage);
+
+        app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}...`));
+    })
+    .catch(error => console.error(error));
